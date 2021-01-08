c300ca677a72b56f85bb21b2b20d81d4dcbb2e21
this-is-shashank-mishra <geekyholic2275@gmail.com>
2021-01-08 21:24:49 +0530

    add Card component

---
+++ src/components/card/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+// eslint-disable-next-line
+import card from "./card.module.scss";
+import { FiBookmark } from "react-icons/fi";
+
+export default function Card({ name, price, image }) {
+  return (
+    <div className={card.container}>
+      <img className={card.image} src={image} />
+      <p className={card.name}>{name}</p>
+      <div className={card.lowerSection}>
+      <p className={card.price}>₹{price}</p>
+      <FiBookmark className={card.icon} />
+      </div>
+    </div>
+  );
+}
