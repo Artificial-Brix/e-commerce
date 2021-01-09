b60f83aef2e07c6598c4e4281d3f3d1eb77e4a75
PratikDey <56970651+PratikDey@users.noreply.github.com>
2021-01-09 22:13:24 +0530

    changed h1 from Introducing Git to pratikkkk

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -8,9 +8,7 @@ export default function navbar() {
   return (
     <nav>
       <div className={classes.Navbar}>
-        <h1 className={classes.Heading}>
-          EcommerceApp - Introducing Git Testing....
-        </h1>
+        <h1 className={classes.Heading}>EcommerceApp - pratikkkkkkk</h1>
 
         <div className={classes.HeaderRight}>
           <form>
